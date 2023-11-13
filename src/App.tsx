3d38c835fa9036e6221a78556c558cb5ff4aaa56
dalogsamurai <thelogsamurai@gmail.com>
2023-11-13 08:38:22 +0300

    feat: Supabase init

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,14 @@ import { RouteObject, useRoutes } from "react-router-dom";
 import { TABLE_PATH } from "./routes/paths";
 import MainPage from "./pages/main.page";
 import TablePage from "./pages/table.page";
+import { createClient } from "@supabase/supabase-js";
 
 function App() {
+	const supabaseUrl = process.env.REACT_APP_SUPABASE_URL!;
+	const supabaseKey = process.env.REACT_APP_SUPABASE_API_KEY!;
+
+	const supabase = createClient(supabaseUrl, supabaseKey);
+
 	const routes: RouteObject[] = [
 		{
 			path: "/",
