225d217ea6ec164ad55668ececca93937210d666
dalogsamurai <thelogsamurai@gmail.com>
2023-11-13 21:44:30 +0300

    feat: get data wip

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,30 @@ import { TABLE_PATH } from "./routes/paths";
 import MainPage from "./pages/main.page";
 import TablePage from "./pages/table.page";
 import { createClient } from "@supabase/supabase-js";
+import { useEffect } from "react";
 
 function App() {
 	const supabaseUrl = process.env.REACT_APP_SUPABASE_URL!;
 	const supabaseKey = process.env.REACT_APP_SUPABASE_API_KEY!;
 
 	const supabase = createClient(supabaseUrl, supabaseKey);
+
+	console.log(supabase);
+
+	const getUserTable = async () => {
+		const { data } = await supabase.from("user").select("");
+		console.log(data);
+	};
+
+	const getRoleTable = async () => {
+		const { data } = await supabase.from("role").select();
+		console.log(data);
+	};
+
+	useEffect(() => {
+		getUserTable();
+		getRoleTable();
+	}, []);
 
 	const routes: RouteObject[] = [
 		{
