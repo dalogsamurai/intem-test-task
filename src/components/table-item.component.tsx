2eb18820f01a2e58fae8e9ffcd1a2dd9532badc7
dalogsamurai <thelogsamurai@gmail.com>
2023-11-14 15:37:11 +0300

    fix(TableItem): display value fix

--- src/components/table-item.component.tsx
+++ src/components/table-item.component.tsx
@@ -9,13 +9,14 @@ const TableItem = ({ item }: Props) => {
 	const [tableValues, setTableValues] = useState<string[]>([]);
 
 	useEffect(() => {
+		console.log(Object.values(item));
 		setTableValues(Object.values(item));
 	}, [item]);
 
 	return (
 		<>
 			{tableValues.map((value) => (
-				<td style={{ width: "200px", cursor: "pointer" }}>{value}</td>
+				<td style={{ width: "200px", cursor: "pointer" }}>{`${value}`}</td>
 			))}
 		</>
 	);
